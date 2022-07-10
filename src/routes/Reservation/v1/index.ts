8e8128dbe0cb3e760b23d14f9e7e8b1c68819068
Halil İbrahim Özdoğan <hiozdogan2@gmail.com>
2022-07-10 17:37:50 +0200

    local-db-volume file removed added to gitignore to not push

--- src/routes/Reservation/v1/index.ts
+++ src/routes/Reservation/v1/index.ts
@@ -10,11 +10,21 @@ export default function (route: Router) {
         const result = await reservationRepository.insert(req.body);
 
         if (result === -1) {
-            res.status(400).send({ status: false, message: 'validation error'});
+            res.status(400).send({ status: false, message: 'validation error' });
         } else if (result === 0) {
             res.status(500).send({ status: false, message: 'unexpected error' });
         } else {
-            res.status(200). send({status: true, message: 'success' });
+            res.status(200).send({ status: true, message: 'success' });
+        }
+    });
+
+    route.get('/reservations', async (req: Request, res: Response) => {
+        try {
+            const reservations = await reservationRepository.find();
+
+            res.status(200).send({ status: true, reservations });
+        } catch (e) {
+            res.status(500).send({ status: false, message: 'unexpected error' });
         }
     });
 }
